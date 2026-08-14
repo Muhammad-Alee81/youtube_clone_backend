33f08ee1ebd6cee2de3272c57294a040f4fa1e2c
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-08-14 19:02:57 +0500

    refactor: improved small bug

--- src/controllers/watchHistory.controller.js
+++ src/controllers/watchHistory.controller.js
@@ -1,5 +1,25 @@
+import mongoose from "mongoose";
 import { catchAsync } from "../utils/catch_async.js";
+import ApiError from "../utils/api_error.js";
+import { WatchHistory } from "../models/history.model.js";
 
 export const addVideoToWatchHistory = catchAsync(async (req, res, next) => {
+        const { videoId } = req.params;
+
+        if (req.user?.id) {
+                return next(new ApiError("unauthorized", 403));
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(videoId)) {
+                return next(new ApiError("Invalid Id", 400));
+        }
+
+        // const addToHistory = await WatchHistory.findOneAndUpdate({
+        //         video: videoId,
+        //         user: req.user.id,
+        // } , {
+        //   upsert:
+        // });
+
         return res.status(201).json({ status: "success" });
 });
