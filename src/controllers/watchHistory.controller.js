cf28e5d56b4ba1e4f0cbd73c966706605d05ab51
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-08-17 22:03:22 +0500

    feat: implemented add video in watch history & delete video from watch history

--- src/controllers/watchHistory.controller.js
+++ src/controllers/watchHistory.controller.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 import { catchAsync } from "../utils/catch_async.js";
 import ApiError from "../utils/api_error.js";
 import { WatchHistory } from "../models/history.model.js";
+import { Video } from "../models/video.model.js";
 
 export const addVideoToWatchHistory = catchAsync(async (req, res, next) => {
         const { videoId } = req.params;
 
-        if (req.user?.id) {
+        if (!req.user?.id) {
                 return next(new ApiError("unauthorized", 403));
         }
 
@@ -14,12 +15,60 @@ export const addVideoToWatchHistory = catchAsync(async (req, res, next) => {
                 return next(new ApiError("Invalid Id", 400));
         }
 
-        // const addToHistory = await WatchHistory.findOneAndUpdate({
-        //         video: videoId,
-        //         user: req.user.id,
-        // } , {
-        //   upsert:
-        // });
+        const video = await Video.findById(videoId);
+
+        if (!video) {
+                return next(new ApiError("Video not found", 404));
+        }
+
+        const addToHistory = await WatchHistory.findOneAndUpdate(
+                {
+                        video: videoId,
+                        user: req.user?.id,
+                },
+
+                {
+                        $set: {
+                                watchedAt: new Date(),
+                        },
+                        $setOnInsert: {
+                                video: videoId,
+                                user: req.user.id,
+                        },
+                },
+
+                {
+                        upsert: true,
+                        returnDocument: "after",
+                }
+        );
 
         return res.status(201).json({ status: "success" });
+});
+
+export const removeVideoFromHistory = catchAsync(async (req, res, next) => {
+        const { videoId } = req.params;
+
+        if (!req.user?.id) {
+                return next(new ApiError("unauthorized", 401));
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(videoId)) {
+                return next(new ApiError("Invalid Id", 400));
+        }
+
+        const history = await WatchHistory.findOneAndDelete({
+                video: videoId,
+                user: req.user.id,
+        });
+
+        if (!history) {
+                return next(
+                        new ApiError("Video not found in watch history", 404)
+                );
+        }
+
+        return res
+                .status(200)
+                .json({ message: "video removed from watch History" });
 });
--- src/routes/history.routes.js
+++ src/routes/history.routes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { addVideoToWatchHistory } from "../controllers/watchHistory.controller.js";
+import {
+        addVideoToWatchHistory,
+        removeVideoFromHistory,
+} from "../controllers/watchHistory.controller.js";
+import { checkAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/:videoId").post(addVideoToWatchHistory);
+router.route("/:videoId")
+        .post(checkAuth, addVideoToWatchHistory)
+        .delete(checkAuth, removeVideoFromHistory);
 
 export default router;
