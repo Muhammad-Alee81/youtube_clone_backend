52d85ea0852d4929d2113643921f0bb7074c0dc8
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-08-19 18:41:00 +0500

    feat(dashboard): add video stats functionality

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,5 +1,9 @@
 import { catchAsync } from "../utils/catch_async.js";
 import ApiError from "../utils/api_error.js";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+
+import mongoose from "mongoose";
 /*
 
 1) total views 
@@ -14,5 +18,44 @@ export const getStats = catchAsync(async (req, res, next) => {
                 return next(new ApiError("unauthorized", 401));
         }
 
-        return res.status(200).json({ status: "success" });
+        const [subscribers, stats] = await Promise.all([
+                Subscription.countDocuments({
+                        channel: req.user.id,
+                }),
+
+                Video.aggregate([
+                        {
+                                $match: {
+                                        owner: new mongoose.Types.ObjectId(
+                                                req.user.id
+                                        ),
+                                },
+                        },
+                        {
+                                $group: {
+                                        _id: null,
+                                        totalVideos: { $sum: 1 },
+                                        totalViews: { $sum: "$views" },
+                                        totalLikes: { $sum: "$likesCount" },
+                                        totalComments: {
+                                                $sum: "$commentsCount",
+                                        },
+                                },
+                        },
+                ]),
+        ]);
+        const videoStats = stats[0] || {
+                totalVideos: 0,
+                totalViews: 0,
+                totalLikes: 0,
+                totalComments: 0,
+        };
+
+        return res.status(200).json({
+                status: "success",
+                stats: {
+                        ...videoStats,
+                        totalSubscribers: subscribers,
+                },
+        });
 });
