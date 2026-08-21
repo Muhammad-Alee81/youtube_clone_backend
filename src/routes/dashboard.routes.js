ad5d31403cbcccc2f4b42fef17809052506fc08d
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-08-21 21:03:57 +0500

    feat(Dashboard): added top user videos functionality

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { checkAuth } from "../middlewares/auth.middleware.js";
-import { getStats } from "../controllers/dashboard.controller.js";
+import { getStats, getTopVideos } from "../controllers/dashboard.controller.js";
 
 const router = express.Router();
 
 router.use(checkAuth);
 
 router.route("/stats").get(getStats);
+router.route("/top-videos").get(getTopVideos);
 
 export default router;
