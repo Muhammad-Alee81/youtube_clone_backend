eba910f4e481bdfeda0c826353b7998b455b985e
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-08-22 01:53:41 +0500

    refactor(Dashboard): added some changes on dashboard routes

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -12,6 +12,10 @@ const router = express.Router();
 router.use(checkAuth);
 
 router.route("/stats").get(getStats);
+
+// http://localhost:4000/api/v1/dashboard/top-videos?sort=views
+// http://localhost:4000/api/v1/dashboard/top-videos?sort=likesCount
+// http://localhost:4000/api/v1/dashboard/top-videos?sort=commentsCount
 router.route("/top-videos").get(
         validation(queryValidation),
         getTopVideos
