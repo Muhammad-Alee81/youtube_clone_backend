5ac06749104c184aac0fcec2ed38cd55c73f4e87
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-08-30 20:46:17 +0500

    refactor: optimized playlist controllers

--- app_architecture/allApis.md
+++ app_architecture/allApis.md
@@ -1,8 +1,59 @@
-Apis for User
 
 
-01. Get All Videos
+01. Get All Videos (supports pagination) (for loggedIn user as well as Guest user)
 http://localhost:4000/api/v1/videos/all?
 
-Get All Subscriptions
-02. http://localhost:4000/api/v1
+
+--------------------------------------------------------------------------
+02. SUBSCRIPTION APIS
+---------------------------------------------------------------------
+
+Get All Subscriptions (supports pagination) (for authenticated user)
+01. http://localhost:4000/api/v1/subscriptions/my
+
+--------------------------------------------------------------------------
+
+
+----------------------------------------
+03. Users Watch History Apis 
+---------------------------------------
+
+Get users watch history(supports pagination)
+01. (GET) /watch-history/
+
+Delete Video from watch History
+02. (DELETE) /watch-history/:videoId
+
+Clear all History 
+03. (DELETE) /watch-history/clear-history
+
+ Add video to Watch History
+ 04. (POST) /watch-history/:videoId
+
+ ----------------------------------------------------------------------
+
+
+----------------------------------------------------------------
+04. PLAYLIST APIS (authentication required)
+---------------------------------------------------------------
+
+create playlist
+01. (POST)  /playlists/ (authenticated user only)
+
+get users playlists
+02. (GET) /users/:userId/playlists
+
+add video to playlist
+03. (PATCH) /playlists/add/:videoId/:playlistId
+
+remove video from playlist
+04. /remove/:videoId/:playlistId
+
+update Playlist 
+05. /playlists/:playlistId
+
+delete playlist
+06. (DELETE)/playlists/:playlistId
+
+get playlist by id
+07. (GET) /playlists/:id
--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -6,207 +6,229 @@ import { catchAsync } from "../utils/catch_async.js";
 import { Video } from "../models/video.model.js";
 
 export const createPlaylist = catchAsync(async (req, res, next) => {
-        const { title, description, visibility } = req.body;
+    const { title, description, visibility } = req.body;
 
-        if (!title) {
-                return next(new ApiError("title is required", 400));
-        }
+    if (!title) {
+        return next(new ApiError("title is required", 400));
+    }
 
-        if (!req.user) {
-                return next(new ApiError("unauthorized", 403));
-        }
+    if (!req.user) {
+        return next(new ApiError("unauthorized", 403));
+    }
 
-        const playlist = await Playlist.create({
-                title,
-                description: description || "",
-                owner: req.user?.id,
-                visibility,
-        });
+    const playlist = await Playlist.create({
+        title,
+        description: description || "",
+        owner: req.user?.id,
+        visibility,
+    });
 
-        return res.status(201).json({ message: "playlist created", playlist });
+    return res.status(201).json({
+        message: "playlist created",
+        playlist: {
+            _id: playlist._id,
+            title: playlist.title,
+            description: playlist.description,
+            visibility: playlist.visibility,
+            owner: playlist.owner,
+            videos: playlist.videos,
+            createdAt: playlist.createdAt,
+        },
+    });
 });
 
 export const getUsersPlaylist = catchAsync(async (req, res, next) => {
-        if (!mongoose.Types.ObjectId.isValid(req.params.userId)) {
-                return next(new ApiError("Invalid user id", 400));
-        }
+    if (!mongoose.Types.ObjectId.isValid(req.params.userId)) {
+        return next(new ApiError("Invalid user id", 400));
+    }
 
-        let playLists;
-        if (req.params.userId !== req.user.id) {
-                playLists = await Playlist.find({
-                        owner: req.params.userId,
-                        visibility: "public",
-                }).sort("-createdAt");
-        } else {
-                playLists = await Playlist.find({
-                        owner: req.params.userId,
-                }).sort("-createdAt");
-        }
+    let playLists;
+    if (req.params.userId !== req.user.id) {
+        playLists = await Playlist.find({
+            owner: req.params.userId,
+            visibility: "public",
+        }).sort("-createdAt");
+    } else {
+        playLists = await Playlist.find({
+            owner: req.params.userId,
+        }).sort("-createdAt");
+    }
 
-        return res.status(200).json({ count: playLists.length, playLists });
+    const data = playLists.map((el) => {
+        const playlist = el;
+        return {
+            playlist: {
+                _id: playlist._id,
+                title: playlist.title,
+                description: playlist.description,
+                visibility: playlist.visibility,
+                owner: playlist.owner,
+                videos: playlist.videos,
+                createdAt: playlist.createdAt,
+            },
+        };
+    });
+
+    return res.status(200).json({
+        status: "success",
+        result: playLists.length,
+        data,
+    });
 });
 
 export const getPlayListById = catchAsync(async (req, res, next) => {
-        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-                return next(new ApiError("Invalid Id", 400));
-        }
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return next(new ApiError("Invalid Id", 400));
+    }
 
-        const playList = await Playlist.findById(req.params.id);
+    const playList = await Playlist.findById(req.params.id);
 
-        if (!playList) {
-                return next(new ApiError("playlist not found", 404));
-        }
+    if (!playList) {
+        return next(new ApiError("playlist not found", 404));
+    }
 
-        const isOwner = playList.owner.equals(req.params.id);
+    const isOwner = playList.owner.equals(req.params.id);
 
-        if (!isOwner && playList.visibility === "private") {
-                return next(new ApiError("playList not found", 403));
-        }
+    if (!isOwner && playList.visibility === "private") {
+        return next(new ApiError("playList not found", 403));
+    }
 
-        return res.status(200).json({ status: "success", playList });
+    return res.status(200).json({ status: "success", playList });
 });
 
 export const addVideoToPlaylist = catchAsync(async (req, res, next) => {
-        const { videoId, playlistId } = req.params;
+    const { videoId, playlistId } = req.params;
 
-        if (!videoId || !playlistId) {
-                return next(
-                        new ApiError("video Id or playlist Id is required", 400)
-                );
-        }
+    if (!videoId || !playlistId) {
+        return next(new ApiError("video Id or playlist Id is required", 400));
+    }
 
-        if (
-                !mongoose.Types.ObjectId.isValid(videoId) ||
-                !mongoose.Types.ObjectId.isValid(playlistId)
-        ) {
-                return next(new ApiError("Invalid Id", 400));
-        }
+    if (
+        !mongoose.Types.ObjectId.isValid(videoId) ||
+        !mongoose.Types.ObjectId.isValid(playlistId)
+    ) {
+        return next(new ApiError("Invalid Id", 400));
+    }
 
-        const video = await Video.findById(videoId);
+    const video = await Video.findById(videoId);
 
-        if (!video) {
-                return next(new ApiError("Video not found", 404));
-        }
+    if (!video) {
+        return next(new ApiError("Video not found", 404));
+    }
 
-        const playList = await Playlist.findOneAndUpdate(
-                {
-                        _id: playlistId,
-                        owner: req.user.id,
-                },
-                {
-                        $addToSet: {
-                                videos: new mongoose.Types.ObjectId(videoId),
-                        },
-                },
-                { returnDocument: "after" }
-        );
+    const playList = await Playlist.findOneAndUpdate(
+        {
+            _id: playlistId,
+            owner: req.user.id,
+        },
+        {
+            $addToSet: {
+                videos: new mongoose.Types.ObjectId(videoId),
+            },
+        },
+        { returnDocument: "after" }
+    );
 
-        if (!playList) {
-                return next(new ApiError("playlist not found", 404));
-        }
+    if (!playList) {
+        return next(new ApiError("playlist not found", 404));
+    }
 
-        return res.status(200).json({
-                message: "video added to playlist",
-                playList: playList,
-        });
+    return res.status(200).json({
+        message: "video added to playlist",
+        playList: playList,
+    });
 });
 
 export const removeVideoFromPlaylist = catchAsync(async (req, res, next) => {
-        const { videoId, playlistId } = req.params;
+    const { videoId, playlistId } = req.params;
 
-        if (!videoId || !playlistId) {
-                return next(
-                        new ApiError("video Id or playlist Id is required", 400)
-                );
-        }
+    if (!videoId || !playlistId) {
+        return next(new ApiError("video Id or playlist Id is required", 400));
+    }
 
-        if (
-                !mongoose.Types.ObjectId.isValid(videoId) ||
-                !mongoose.Types.ObjectId.isValid(playlistId)
-        ) {
-                return next(new ApiError("Invalid Id", 400));
-        }
+    if (
+        !mongoose.Types.ObjectId.isValid(videoId) ||
+        !mongoose.Types.ObjectId.isValid(playlistId)
+    ) {
+        return next(new ApiError("Invalid Id", 400));
+    }
 
-        const video = await Video.findById(videoId);
+    const video = await Video.findById(videoId);
 
-        if (!video) {
-                return next(new ApiError("Video not found", 404));
-        }
+    if (!video) {
+        return next(new ApiError("Video not found", 404));
+    }
 
-        const playList = await Playlist.findOneAndUpdate(
-                {
-                        _id: playlistId,
-                        owner: req.user.id,
-                },
-                {
-                        $pull: {
-                                videos: videoId,
-                        },
-                },
-                { returnDocument: "after" }
-        );
+    const playList = await Playlist.findOneAndUpdate(
+        {
+            _id: playlistId,
+            owner: req.user.id,
+        },
+        {
+            $pull: {
+                videos: videoId,
+            },
+        },
+        { returnDocument: "after" }
+    );
 
-        if (!playList) {
-                return next(new ApiError("Playlist not found", 404));
-        }
+    if (!playList) {
+        return next(new ApiError("Playlist not found", 404));
+    }
 
-        return res.status(200).json({ status: "success", playList });
+    return res.status(200).json({ status: "success", playList });
 });
 
 export const deletePlayList = catchAsync(async (req, res, next) => {
-        const { id } = req.params;
+    const { id } = req.params;
 
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-                return next(new ApiError("Invalid Id", 400));
-        }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return next(new ApiError("Invalid Id", 400));
+    }
 
-        const deletedPlayList = await Playlist.findOneAndDelete({
-                _id: id,
-                owner: req.user.id,
-        });
+    const deletedPlayList = await Playlist.findOneAndDelete({
+        _id: id,
+        owner: req.user.id,
+    });
 
-        if (!deletedPlayList) {
-                return next(new ApiError("Playlist not found", 404));
-        }
+    if (!deletedPlayList) {
+        return next(new ApiError("Playlist not found", 404));
+    }
 
-        return res
-                .status(200)
-                .json({ message: "playlist deleted successfully" });
+    return res.status(200).json({ message: "playlist deleted successfully" });
 });
 
 export const updatePlaylist = catchAsync(async (req, res, next) => {
-        const { title, description, visibility } = req.body;
-        const { id } = req.params;
+    const { title, description, visibility } = req.body;
+    const { id } = req.params;
 
-        if (title !== undefined && title.trim() === "") {
-                return next(new ApiError("Title cannot be empty", 400));
+    if (title !== undefined && title.trim() === "") {
+        return next(new ApiError("Title cannot be empty", 400));
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return next(new ApiError("Invalid Id", 400));
+    }
+
+    const updatedPlayList = await Playlist.findOneAndUpdate(
+        {
+            _id: id,
+            owner: req.user.id,
+        },
+        {
+            $set: {
+                ...(title !== undefined && { title }),
+                ...(description !== undefined && {
+                    description,
+                }),
+                ...(visibility !== undefined && { visibility }),
+            },
         }
+    );
 
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-                return next(new ApiError("Invalid Id", 400));      
-        }
+    if (!updatedPlayList) {
+        return next(new ApiError("playlist is not found", 404));
+    }
 
-        const updatedPlayList = await Playlist.findOneAndUpdate(
-                {
-                        _id: id,
-                        owner: req.user.id,
-                },
-                {
-                        $set: {
-                                ...(title !== undefined && { title }),
-                                ...(description !== undefined && {
-                                        description,
-                                }),
-                                ...(visibility !== undefined && { visibility }),
-                        },
-                }
-        );
-
-        if (!updatedPlayList) {
-                return next(new ApiError("playlist is not found", 404));
-        }
-
-        return res
-                .status(200)
-                .json({ message: "playlist updated successfully" });
+    return res.status(200).json({ message: "playlist updated successfully" });
 });
