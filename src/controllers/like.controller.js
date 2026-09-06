e8c80bb13ecc950b8240777dc656b76c13d7a29f
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-09-06 12:22:07 +0500

    feat: optimized other all controllers response

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -8,193 +8,212 @@ import { Post } from "../models/posts.model.js";
 import { pagination } from "../utils/aggreegation/pagination.js";
 
 export const toggleVideoLike = catchAsync(async (req, res, next) => {
-        const { videoId } = req.params;
+    const { videoId } = req.params;
 
-        if (!req.user?.id) {
-                return next(new ApiError("unauthorized", 400));
-        }
+    if (!req.user?.id) {
+        return next(new ApiError("unauthorized", 400));
+    }
 
-        if (!mongoose.Types.ObjectId.isValid(videoId)) {
-                return next(new ApiError("Invalid Id", 400));
-        }
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        return next(new ApiError("Invalid Id", 400));
+    }
 
-        const video = await Video.exists(new mongoose.Types.ObjectId(videoId));
+    const video = await Video.exists(new mongoose.Types.ObjectId(videoId));
 
-        if (!video) {
-                return next(new ApiError("Video not found", 404));
-        }
+    if (!video) {
+        return next(new ApiError("Video not found", 404));
+    }
 
-        const checkLike = await Like.findOne({
-                video: videoId,
-                likedBy: req.user.id,
+    const checkLike = await Like.findOne({
+        video: videoId,
+        likedBy: req.user.id,
+    });
+
+    if (checkLike) {
+        await Like.findByIdAndDelete(checkLike._id);
+        await Video.findByIdAndUpdate(
+            { _id: videoId },
+            { $inc: { likesCount: -1 } }
+        );
+    } else {
+        await Like.create({
+            video: videoId,
+            likedBy: req.user.id,
         });
 
-        if (checkLike) {
-                await Like.findByIdAndDelete(checkLike._id);
-                await Video.findByIdAndUpdate(
-                        { _id: videoId },
-                        { $inc: { likesCount: -1 } }
-                );
-        } else {
-                await Like.create({
-                        video: videoId,
-                        likedBy: req.user.id,
-                });
+        await Video.findByIdAndUpdate(
+            { _id: videoId },
+            { $inc: { likesCount: 1 } }
+        );
+    }
 
-                await Video.findByIdAndUpdate(
-                        { _id: videoId },
-                        { $inc: { likesCount: 1 } }
-                );
-        }
-
-        return res.status(200).json({ status: "success" });
+    return res.status(200).json({ status: "success" });
 });
 
 export const toggleCommentLike = catchAsync(async (req, res, next) => {
-        const { commentId } = req.params;
+    const { commentId } = req.params;
 
-        if (!req.user?.id) {
-                return next(new ApiError("unauthorized", 400));
-        }
+    if (!req.user?.id) {
+        return next(new ApiError("unauthorized", 400));
+    }
 
-        if (!mongoose.Types.ObjectId.isValid(commentId)) {
-                return next(new ApiError("Invalid Id", 400));
-        }
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
+        return next(new ApiError("Invalid Id", 400));
+    }
 
-        const comment = await Comment.exists(
-                new mongoose.Types.ObjectId(commentId)
-        );
+    const comment = await Comment.exists(
+        new mongoose.Types.ObjectId(commentId)
+    );
 
-        if (!comment) {
-                return next(new ApiError("Comment not found", 404));
-        }
+    if (!comment) {
+        return next(new ApiError("Comment not found", 404));
+    }
 
-        const checkLike = await Like.findOne({
-                comment: commentId,
-                likedBy: req.user.id,
+    const checkLike = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user.id,
+    });
+
+    if (checkLike) {
+        await Like.findByIdAndDelete(checkLike._id);
+    } else {
+        await Like.create({
+            comment: commentId,
+            likedBy: req.user.id,
         });
+    }
 
-        if (checkLike) {
-                await Like.findByIdAndDelete(checkLike._id);
-        } else {
-                await Like.create({
-                        comment: commentId,
-                        likedBy: req.user.id,
-                });
-        }
-
-        return res.status(200).json({ status: "success" });
+    return res.status(200).json({ status: "success" });
 });
 
 export const togglePostLike = catchAsync(async (req, res, next) => {
-        const { postId } = req.params;
+    const { postId } = req.params;
 
-        if (!req.user?.id) {
-                return next(new ApiError("unauthorized", 400));
-        }
+    if (!req.user?.id) {
+        return next(new ApiError("unauthorized", 400));
+    }
 
-        if (!mongoose.Types.ObjectId.isValid(postId)) {
-                return next(new ApiError("Invalid Id", 400));
-        }
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        return next(new ApiError("Invalid Id", 400));
+    }
 
-        const post = await Post.exists(new mongoose.Types.ObjectId(postId));
+    const post = await Post.exists(new mongoose.Types.ObjectId(postId));
 
-        if (!post) {
-                return next(new ApiError("Post not found", 404));
-        }
+    if (!post) {
+        return next(new ApiError("Post not found", 404));
+    }
 
-        const checkLike = await Like.findOne({
-                post: postId,
-                likedBy: req.user.id,
+    const checkLike = await Like.findOne({
+        post: postId,
+        likedBy: req.user.id,
+    });
+
+    if (checkLike) {
+        await Like.findByIdAndDelete(checkLike._id);
+    } else {
+        await Like.create({
+            post: postId,
+            likedBy: req.user.id,
         });
+    }
 
-        if (checkLike) {
-                await Like.findByIdAndDelete(checkLike._id);
-        } else {
-                await Like.create({
-                        post: postId,
-                        likedBy: req.user.id,
-                });
-        }
-
-        return res.status(200).json({ status: "success" });
+    return res.status(200).json({ status: "success" });
 });
 
 export const getAllLikedVideos = catchAsync(async (req, res, next) => {
-        const { page, limit } = req.validateQuery;
+    const { page, limit } = req.validateQuery;
 
-        if (!req.user?.id) {
-                return next(new ApiError("unauthorized", 403));
-        }
+    if (!req.user?.id) {
+        return next(new ApiError("unauthorized", 403));
+    }
 
-        const pipeline = [];
+    const pipeline = [];
 
-        pipeline.push(
-                {
-                        $match: {
-                                likedBy: new mongoose.Types.ObjectId(
-                                        req.user.id
-                                ),
-                        },
-                },
-                {
-                        $lookup: {
-                                from: "videos",
-                                foreignField: "_id",
-                                localField: "video",
-                                as: "videos",
-                                pipeline: [
-                                        {
-                                                $lookup: {
-                                                        from: "users",
-                                                        foreignField: "_id",
-                                                        localField: "owner",
-                                                        as: "owner",
-                                                        pipeline: [
-                                                                {
-                                                                        $project: {
-                                                                                username: 1,
-                                                                        },
-                                                                },
-                                                        ],
-                                                },
-                                        },
+    pipeline.push(
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user.id),
+            },
+        },
 
-                                        {
-                                                $unwind: "$owner",
-                                        },
-
-                                        {
-                                                $project: {
-                                                        title: 1,
-                                                        description: 1,
-                                                        thumbnail: 1,
-                                                        owner: 1,
-                                                        createdAt: 1,
-                                                        updatedAt: 1,
-                                                        views: 1,
-                                                },
-                                        },
-                                ],
-                        },
-                },
-
-                {
-                        $unwind: "$videos",
-                },
-
-                {
+        {
+            $lookup: {
+                from: "users",
+                foreignField: "_id",
+                localField: "likedBy",
+                as: "likedBy",
+                pipeline: [
+                    {
                         $project: {
-                                video: 1,
-                                likedBy: 1,
-                                videos: 1,
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1,
                         },
-                }
-        );
+                    },
+                ],
+            },
+        },
 
-        pagination({ page, limit, pipeline, totalCount: "totalLikedVideos" });
+        { $unwind: "$likedBy" },
 
-        const likedVideos = await Like.aggregate(pipeline);
+        {
+            $lookup: {
+                from: "videos",
+                foreignField: "_id",
+                localField: "video",
+                as: "videos",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            foreignField: "_id",
+                            localField: "owner",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                    },
+                                },
+                            ],
+                        },
+                    },
 
-        return res.status(200).json({ status: "success", likedVideos });
+                    {
+                        $unwind: "$owner",
+                    },
+
+                    {
+                        $project: {
+                            title: 1,
+                            description: 1,
+                            thumbnail: 1,
+                            owner: 1,
+                            createdAt: 1,
+                            updatedAt: 1,
+                            views: 1,
+                        },
+                    },
+                ],
+            },
+        },
+
+        {
+            $unwind: "$videos",
+        },
+
+        {
+            $project: {
+                video: 1,
+                likedBy: 1,
+                videos: 1,
+            },
+        }
+    );
+
+    pagination({ page, limit, pipeline, totalCount: "totalLikedVideos" });
+
+    const likedVideos = await Like.aggregate(pipeline);
+
+    return res.status(200).json({ status: "success", likedVideos });
 });
